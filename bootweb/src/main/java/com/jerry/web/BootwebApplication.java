bd09a8dee807dd1fdb65e94c9838724bc7507e9d
jerry <jerry110160@gmail.com>
2019-04-06 20:43:05 -0400

    Add Spring boot project

---
+++ bootweb/src/main/java/com/jerry/web/BootwebApplication.java
@@ -0,0 +1,13 @@
+package com.jerry.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootwebApplication.class, args);
+	}
+
+}
